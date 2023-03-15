b1dcb2af753cccaeb9fc2b9925aaca8acfb7d8b9
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-15 14:18:44 +0300

    setup docker for react

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reloadinging.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
+        <p>{`Hello ${process.env.REACT_APP_NAME}`}</p>
+        <p>hello there</p>
       </header>
     </div>
   );
 }
-
 export default App;
